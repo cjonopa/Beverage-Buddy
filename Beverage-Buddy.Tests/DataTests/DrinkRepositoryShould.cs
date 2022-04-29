dddf3d28f2edeace651aa6f6bfd828648d445770
Christopher Onopa <cjonopa@uwm.edu>
2022-04-29 12:58:45 -0500

    Add Drink tests added

--- Beverage-Buddy.Tests/DataTests/DrinkRepositoryShould.cs
+++ Beverage-Buddy.Tests/DataTests/DrinkRepositoryShould.cs
@@ -48,5 +48,83 @@ namespace Beverage_Buddy.Tests.DataTests
             Assert.IsAssignableFrom<ICollection<Drink>>(result);
             Assert.Equal(expected, result.Count);
         }
+
+        [Fact]
+        public async void Add_A_Drink()
+        {
+            //-- Arrange
+            mockDrinkRepo.Setup(mdr => mdr.GetAllAsync())
+                .Returns(Task.FromResult(drinks));
+            var drink = new Drink { Id = "3", DrinkName = "Drink 3" };
+            var repo = mockDrinkRepo.Object;
+
+            //-- Act
+            var result = await repo.GetAllAsync();
+            result.Add(drink);
+
+            //-- Assert
+            Assert.IsAssignableFrom<ICollection<Drink>>(result);
+            Assert.Equal(expected + 1, result.Count);
+        }
+
+        [Fact]
+        public async void Add_Multiple_Drinks()
+        {
+            //-- Arrange
+            mockDrinkRepo.Setup(mdr => mdr.GetAllAsync())
+                .Returns(Task.FromResult(drinks));
+            var drink1 = new Drink { Id = "3", DrinkName = "Drink 3" };
+            var drink2 = new Drink { Id = "4", DrinkName = "Drink 4" };
+            var repo = mockDrinkRepo.Object;
+
+            //-- Act
+            var result = await repo.GetAllAsync();
+            result.Add(drink1);
+            result.Add(drink2);
+
+
+            //-- Assert
+            Assert.IsAssignableFrom<ICollection<Drink>>(result);
+            Assert.Equal(expected + 2, result.Count);
+        }
+       
+        [Fact]
+        public async void Delete_A_Drink()
+        {
+            //-- Arrange
+            mockDrinkRepo.Setup(mdr => mdr.GetAllAsync())
+                .Returns(Task.FromResult(drinks));
+            var repo = mockDrinkRepo.Object;
+
+            //-- Act
+            repo.Delete("2");
+            var result = await repo.GetAllAsync();
+            
+
+            //-- Assert
+            Assert.IsAssignableFrom<ICollection<Drink>>(result);
+            Assert.Equal(expected - 1, result.Count);
+        }
+
+        [Fact]
+        public async void Update_Drink_Alcoholic()
+        {
+            //-- Arrange
+            mockDrinkRepo.Setup(mdr => mdr.GetAllAsync())
+                .Returns(Task.FromResult(drinks));
+            var repo = mockDrinkRepo.Object;
+            var drink = new Drink { Id = "1", DrinkName = "Drink 1", Alcoholic = true };
+
+            //-- Act
+            repo.Update(drink);
+            //var isAlcoholic = repo.Get("1").Alcoholic.ToString();
+            var result = await repo.GetAllAsync();
+            var contains = await result.Contains(drink);
+            
+
+            //-- Assert
+            Assert.IsAssignableFrom<ICollection<Drink>>(result);
+            Assert.True(result.Contains(drink));
+        }
     }
 }
