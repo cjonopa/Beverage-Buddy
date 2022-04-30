5605fafe15fa3f22f95bf4fca5a44519b73b4bd7
Christopher858 <cddraws@uwm.edu>
2022-04-29 21:48:28 -0500

    Added Tests for RecipeRepository

--- Beverage-Buddy.Tests/DataTests/RecipeRepositoryShould.cs
+++ Beverage-Buddy.Tests/DataTests/RecipeRepositoryShould.cs
@@ -11,9 +11,10 @@ namespace Beverage_Buddy.Tests.DataTests
     public class RecipeRepositoryShould
     {
         private readonly Mock<IRepository<Recipe, string>> mockRecipeRepo;
-
-        private ICollection<Recipe> recipes;
-        private int expected;
+        private Recipe Recipe1 { get; set; }
+        private Recipe Recipe2 { get; set; }
+        private ICollection<Recipe> Recipes { get; set; }
+        private int ExpectedCount { get; set; }
 
         public RecipeRepositoryShould()
         {
@@ -23,13 +24,13 @@ namespace Beverage_Buddy.Tests.DataTests
 
         private void SetupDrinkList()
         {
-            recipes = new List<Recipe>
-            {
-                new Recipe {Id = 1, Name = "Recipe 1", DrinkType = DrinkType.Beer},
-                new Recipe {Id = 2, Name = "Recipe 2", DrinkType = DrinkType.Beer},
-            };
+            Recipe1 = new Recipe { Id = 1, Name = "Recipe 1", DrinkType = DrinkType.Water };
+            Recipe2 = new Recipe { Id = 2, Name = "Recipe 2", DrinkType = DrinkType.Beer };
 
-            expected = 2;
+            Recipes = new List<Recipe>
+            { Recipe1, Recipe2 };
+
+            ExpectedCount = 2;
         }
 
         [Fact]
@@ -37,7 +38,7 @@ namespace Beverage_Buddy.Tests.DataTests
         {
             //-- Arrange
             mockRecipeRepo.Setup(mdr => mdr.GetAllAsync())
-              .Returns(Task.FromResult(recipes));
+              .Returns(Task.FromResult(Recipes));
             var repo = mockRecipeRepo.Object;
 
             //-- Act
@@ -45,24 +46,73 @@ namespace Beverage_Buddy.Tests.DataTests
 
             //-- Assert
             Assert.IsAssignableFrom<ICollection<Recipe>>(result);
-            Assert.Equal(expected, result.Count);
+            Assert.Equal(ExpectedCount, result.Count);
         }
 
         [Fact]
-        public async void Add_A_Recipe_To_The_Collection()
+        public async void Add_A_Recipe_To_The_Repsitory()
         {
             //-- Arrange
-            var AddRecipe = new Recipe { Id = 3, Name = "Recipe 3", DrinkType = DrinkType.Beer};
+            var recipe = new Recipe { Id = 3, Name = "Recipe 3" };
+            mockRecipeRepo.Setup(mdr => mdr.Add(recipe))
+                .Returns(recipe);
+            mockRecipeRepo.Setup(mdr => mdr.SaveAllAsync())
+                .Returns(Task.FromResult(true));
+
             var repo = mockRecipeRepo.Object;
 
             //-- Act
-            try
+            var returned = repo.Add(recipe);
+            var success = await repo.SaveAllAsync();
+
+            //-- Assert
+            Assert.True(success);
+            Assert.IsType<Recipe>(returned);
+            Assert.Equal(recipe, returned);
+        }
+
+        [Fact]
+        public async void Delete_A_Recipe_To_The_Repsitory()
+        {
+            //-- Arrange
+            mockRecipeRepo.Setup(mdr => mdr.Delete("Recipe1"))
+                .Returns(Recipe1);
+            mockRecipeRepo.Setup(mdr => mdr.SaveAllAsync())
+                .Returns(Task.FromResult(true));
+
+            var repo = mockRecipeRepo.Object;
+
+            //-- Act
+            var returned = repo.Delete("Recipe1");
+            var success = await repo.SaveAllAsync();
+
+            //-- Assert
+            Assert.True(success);
+            Assert.IsType<Recipe>(returned);
+            Assert.Equal(Recipe1, returned);
+        }
+
+        [Fact]
+        public async void Update_A_Recipe_To_The_Repsitory()
+        {
             {
-                repo.Add(AddRecipe);
-            }
-            catch 
-            {
-                Assert.False(false);
+                //-- Arrange
+                mockRecipeRepo.Setup(mdr => mdr.Update(Recipe1))
+                    .Returns(Recipe1);
+                mockRecipeRepo.Setup(mdr => mdr.SaveAllAsync())
+                    .Returns(Task.FromResult(true));
+
+                var repo = mockRecipeRepo.Object;
+
+                //-- Act
+                Recipe1.DrinkType = DrinkType.Wine;
+                var returned = repo.Update(Recipe1);
+                var success = await repo.SaveAllAsync();
+
+                //-- Assert
+                Assert.True(success);
+                Assert.IsType<Recipe>(returned);
+                Assert.Equal(Recipe1, returned);
             }
         }
     }
